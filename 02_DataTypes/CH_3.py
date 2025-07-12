977543e53af6c8559a218e0f22dcf0007eaa0dff
Aarav Ahuja <mr.aaravahuja@gmail.com>
2025-07-12 05:31:08 +0000

    making and coding into new CH_3.py file and i will code more in future in this file i think

---
+++ 02_DataTypes/CH_3.py
@@ -0,0 +1,56 @@
+# Integers
+
+# Assign the price of black tea to a variable
+black_tea_price = 20  # Store the price of black tea (20) in a variable called 'black_tea_price'
+
+# Assign the price of ginger tea to a variable
+giger_tea_price = 25  # Store the price of ginger tea (25) in a variable called 'giger_tea_price'
+
+# Calculate the total price of both teas
+Total_tea_price = black_tea_price + giger_tea_price  # Add the prices of black tea and ginger tea, store the result in 'Total_tea_price'
+
+# Print the total tea price
+print("Total tea price:", Total_tea_price)  # Show the total price of both teas on the screen
+
+# Subtract 10 from the total tea price to apply a discount
+remnaining_tea_price = Total_tea_price - 10  # Subtract 10 from the total tea price to get the discounted price, store in 'remnaining_tea_price'
+
+# Print the remaining tea price after discount
+print("Remaining tea price after discount:", remnaining_tea_price)  # Show the discounted price on the screen
+
+# Assign the total liters of milk available
+milk_liters = 7  # Store the total amount of milk (7 liters) in 'milk_liters'
+
+# Assign the number of servings
+servings = 4  # Store the number of servings (4) in 'servings'
+
+# Calculate the amount of milk per serving
+milk_per_serving = milk_liters / servings  # Divide the total milk by the number of servings to get milk per serving
+
+# Print the milk per serving
+print("Milk per serving:", milk_per_serving)  # Show how much milk is used for each serving
+
+# Assign the total number of tea bags
+total_tea_bags = 7  # Store the total number of tea bags (7) in 'total_tea_bags'
+
+# Assign the number of pots
+pots = 4  # Store the number of pots (4) in 'pots'
+
+# Calculate the number of tea bags per pot using integer division but without seeking the decimal part of the value
+bags_per_pot = total_tea_bags // pots  # Divide tea bags by pots, but only keep the whole number (no decimals)
+
+# Print the number of tea bags per pot
+print("Tea bags per pot:", bags_per_pot)  # Show how many tea bags go into each pot
+
+# Assign the total number of cardamom pods
+total_cadamom_pods = 10  # Store the total number of cardamom pods (10) in 'total_cadamom_pods'
+
+# Assign the number of pods needed per cup
+pods_per_cup = 3  # Store the number of pods needed for each cup (3) in 'pods_per_cup'
+
+# Calculate the leftover pods after making as many full cups as possible
+leftover_pods = total_cadamom_pods % pods_per_cup  # Find out how many pods are left after dividing them into cups
+
+# Print the number of leftover cardamom pods
+print("Leftover cardamom pods:", leftover_pods)  # Show how many cardamom pods are left after making cups
+
