aa63fdee6110ffeac8a1de83996b05439bc3cd3a
Aarav Ahuja <mr.aaravahuja@gmail.com>
2025-07-27 09:14:29 +0000

    Remove unused CH_1.py, CH_2.py, CH_3.py, and CH_4.py files to clean up the repository

--- 02_DataTypes/CH_1.py
+++
@@ -1,8 +0,0 @@
-# sugar_amount = 2.5 # teaspoons
-# print("I have", sugar_amount, "teaspoons of sugar.")
-
-# sugar_amount = 12 # teaspoons
-# print("Now I have", sugar_amount, "teaspoons of sugar.")
-
-print(f"ID of 2.5 {id(2.5)}")
-print(f"ID of 12 {id(12)}")
